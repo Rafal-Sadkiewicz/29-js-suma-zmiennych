ffe55799e6d5af08ccce5be0ca343aa8ed82ad62
rafal-sadkiewicz <rafal.sadkiewicz89@gmail.com>
2020-08-26 13:10:59 +0200

    29-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let intFirstNumber = 4, intSecondNumber = 5;
+
+console.log('Pierwsza liczba = ' + intFirstNumber);
+console.log('Druga liczba = ' + intSecondNumber);
+
+function add (intA, intB)
+{
+    return intA+intB;
+}
+
+console.log('Wynik dodawania = ' + add(intFirstNumber,intSecondNumber));
